8074ae8e774472a33f898c960c1010cdd1dda65e
n1h40 <natiqnihad06@gmail.com>
2024-12-19 09:44:36 +0400

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter basename="/Movie-Project">
+  <BrowserRouter>
     <App />
   </BrowserRouter>
 );
